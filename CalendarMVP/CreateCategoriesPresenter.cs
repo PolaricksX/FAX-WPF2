630e27b6ac169131224e86d3f390d1b2a7ac894b
Xavier Mohammed <196205339+PolaricksX@users.noreply.github.com>
2026-04-23 12:26:23 -0400

    Added basic mvp setup

--- CalendarMVP/CreateCategoriesPresenter.cs
+++ CalendarMVP/CreateCategoriesPresenter.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace CalendarMVP
 {
-    internal class View
+    internal class CreateCategoriesPresenter
     {
     }
 }
--- FAX-WPF/MainPresenter.cs
+++ FAX-WPF/MainPresenter.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CalendarMVP
+namespace FAX_WPF
 {
-    internal class View
+    internal class MainPresenterWindow
     {
     }
 }
