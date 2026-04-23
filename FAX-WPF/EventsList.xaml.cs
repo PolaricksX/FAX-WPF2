3ea6c81be5fd1ff631c8f458f6a9f1dfe0114100
Xavier Mohammed <xaviermohammed312@gmail.com>
2026-04-23 09:02:23 -0400

    Add files via upload
    Fixed nested folder structure

---
+++ FAX-WPF/EventsList.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FAX_WPF
+{
+    /// <summary>
+    /// Interaction logic for EventsList.xaml
+    /// </summary>
+    public partial class EventsList : Window
+    {
+        public EventsList()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCreateCategories_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
