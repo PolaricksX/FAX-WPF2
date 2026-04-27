2542187cf2b0bb885ff83c6fff173cada08085e8
Xavier Mohammed <196205339+PolaricksX@users.noreply.github.com>
2026-04-27 09:36:09 -0400

    Merged manually since too many web editor conflicts
    Completed changeable color scheme, pleasant color scheme and closing application story

---
+++ FAX-WPF/EventsPresenter.cs
@@ -0,0 +1,21 @@
+﻿using Calendar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FAX_WPF
+{
+    internal class EventsPresenter
+    {
+        private IEventView _view;
+        private HomeCalendar _model;
+        public EventsPresenter(IEventView v, HomeCalendar m)
+        {
+           _view = v;
+            _model = m;
+        }
+
+    }
+}
---
+++ FAX-WPF/MainPresenter.cs
@@ -0,0 +1,34 @@
+﻿using Calendar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Media3D;
+
+namespace FAX_WPF
+{
+    public class MainPresenter
+    {
+        public IMainView _mainPresenter;
+        private HomeCalendar _model;
+
+        public MainPresenter(IMainView mainView, string filename, bool db)
+        {
+            _mainPresenter = mainView;
+            _model = new HomeCalendar("Default", true);
+        }
+
+        internal EventsPresenter GetEventsPresenter(IEventView ev)
+        {
+            return new EventsPresenter(ev, _model);
+        }
+        
+        
+
+        
+       
+    }
+
+}
+                                            
