1a1b29801fe0c6f2b73838e919d9452d4878b198
Louis-Felix RDC <196205145+Louis-FelixRDC@users.noreply.github.com>
2026-04-29 10:11:00 -0400

    changed interface fields

--- FAX-WPF/IEventView.cs
+++ FAX-WPF/IEventView.cs
@@ -8,7 +8,7 @@ namespace FAX_WPF
 {
     public interface IEventView
     {
-        int CategoryId { get; set; }
+        int EventID { get; set; }
         int DurationMinutes { get; set; }
         DateTime StartDateTime { get; set; }
         string Details { get; set; }
