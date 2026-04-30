56d20de8272d006857c596795d5eede5c24e7420
Alejandro Asido <158082244+AlejandroAlejandroAlejandro@users.noreply.github.com>
2026-04-29 22:48:08 -0400

    Added documentation in CategoryView interface.

--- FAX-WPF/ICategoryView.cs
+++ FAX-WPF/ICategoryView.cs
@@ -7,10 +7,34 @@ using Calendar;
 
 namespace FAX_WPF
 {
+    /// <summary>
+    /// Represents a view for displaying and editing category information within a calendar application.
+    /// </summary>
     public interface ICategoryView
     {
+        /// <summary>
+        /// Gets or sets the description associated with the object.
+        /// </summary>
         string Description { get; set; }
+
+        /// <summary>
+        /// Gets or sets the currently selected category type.
+        /// </summary>
         Calendar.Category.CategoryType SelectedCategoryType { get; set; }
+
+        /// <summary>
+        /// Displays the specified message to the user.
+        /// </summary>
+        /// <param name="message">The message to display. Cannot be null.</param>
+        /// <example>
+        /// <code>
+        /// <![CDATA[
+        /// 
+        /// view.ShowMessage("Success!");
+        /// 
+        /// ]]>
+        /// </code>
+        /// </example>
         void ShowMessage(string message);
     }
 }
